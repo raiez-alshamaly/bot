725755451bfa4806ace10595ef18b9081dd541bd
Raiez Alshamaly <166133102+raiez-alshamaly@users.noreply.github.com>
2025-02-22 11:12:45 -0800

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,10 @@
-
-
 import os
 
 TOKEN = os.getenv("6528215814:AAE8RxzwmBjWw2ngR35gAdVdLPOKd7mFKHU")  # توكن بوت تيليغرام
-DB_URL = os.getenv("postgresql://postgres:YSGTiCZGSyJEyPjOoiLRETKPXjXSccqw@nozomi.proxy.rlwy.net:40923/railway")  # رابط قاعدة بيانات PostgreSQL
+DB_URL = os.getenv("mysql://root:NrvdQyzQmlpuPeiCIiZRfzituhqgRoCu@shinkansen.proxy.rlwy.net:11793/railway")  # رابط قاعدة بيانات MySQL
+
+if not DB_URL:
+    raise ValueError("❌ خطأ: لم يتم تعيين متغير البيئة DB_URL بشكل صحيح!")
+
+# مثال على DB_URL المطلوب:
+# mysql+mysqlconnector://username:password@localhost:3306/telegram_bot_db
