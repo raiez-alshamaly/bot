083f33c3b3bf01acc461b2cb19c6dcc2367a186f
Raiez Alshamaly <166133102+raiez-alshamaly@users.noreply.github.com>
2025-02-22 10:54:40 -0800

    Update config.py

--- config.py
+++ config.py
@@ -3,4 +3,4 @@
 import os
 
 TOKEN = os.getenv("6528215814:AAE8RxzwmBjWw2ngR35gAdVdLPOKd7mFKHU")  # توكن بوت تيليغرام
-DB_URL = os.getenv("postgresql://postgres:TwvzZFqfuaEDKmtUKDxsQUFtVLjgmcXv@trolley.proxy.rlwy.net:42089/railway")  # رابط قاعدة بيانات PostgreSQL
+DB_URL = os.getenv("postgresql://postgres:YSGTiCZGSyJEyPjOoiLRETKPXjXSccqw@nozomi.proxy.rlwy.net:40923/railway")  # رابط قاعدة بيانات PostgreSQL
