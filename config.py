b2c03079509612804897e1cd0a6c5a9144e35371
Raiez Alshamaly <166133102+raiez-alshamaly@users.noreply.github.com>
2025-02-22 10:41:18 -0800

    Update config.py

--- config.py
+++ config.py
@@ -3,4 +3,4 @@
 import os
 
 TOKEN = os.getenv("6528215814:AAE8RxzwmBjWw2ngR35gAdVdLPOKd7mFKHU")  # توكن بوت تيليغرام
-DB_URL = os.getenv("https://railway.com/project/f5b359b7-3524-49c0-b598-e92705d3a54a/service/19dea7c5-9f5c-4914-b6f2-f23d215ecc01?environmentId=c380577c-eefd-4a78-8da4-93e045d6fcfd&id=545f299d-77ca-42e9-8088-efb70e19f955#deploy")  # رابط قاعدة بيانات PostgreSQL
+DB_URL = os.getenv("postgresql://postgres:TwvzZFqfuaEDKmtUKDxsQUFtVLjgmcXv@trolley.proxy.rlwy.net:42089/railway")  # رابط قاعدة بيانات PostgreSQL
